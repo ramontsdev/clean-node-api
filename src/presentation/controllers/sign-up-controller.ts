2f6c1fefc82fd42d492e0bc5d3d4a991e40d93ad
Ramon Nunes <ramontsdev@gmail.com>
2023-02-06 20:40:24 -0300

    refactor

--- src/presentation/controllers/sign-up-controller.ts
+++ src/presentation/controllers/sign-up-controller.ts
@@ -1,7 +1,5 @@
-import { InvalidParamError } from "../errors/invalid-param-error"
-import { MissingParamError } from "../errors/missing-param-error"
-import { ServerError } from "../errors/server-error"
-import { badRequest } from "../helpers/http-helper"
+import { InvalidParamError, MissingParamError } from "../errors"
+import { badRequest, serverError } from "../helpers/http-helper"
 import { Controller } from "../protocols/controller"
 import { EmailValidator } from "../protocols/email-validator"
 import { HttpRequest, HttpResponse } from "../protocols/http"
@@ -30,10 +28,7 @@ export class SignUpController implements Controller {
       }
 
     } catch (error) {
-      return {
-        statusCode: 500,
-        body: new ServerError()
-      }
+      return serverError()
     }
   }
 }
