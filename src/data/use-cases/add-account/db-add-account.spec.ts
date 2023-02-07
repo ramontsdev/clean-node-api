560d64c44542bfba03162194ac29089c877ebdbf
Ramon Nunes <ramontsdev@gmail.com>
2023-02-07 19:55:31 -0300

    refactor: factory

--- src/data/use-cases/add-account/db-add-account.spec.ts
+++ src/data/use-cases/add-account/db-add-account.spec.ts
@@ -1,15 +1,35 @@
-import { DbAddAccount } from "./add-account"
+import { Encrypter } from "../../protocols/encrypter";
+import { DbAddAccount } from "./add-account";
+
+function makeEncrypter(): Encrypter {
+  class EncrypterStub implements Encrypter {
+    async encrypt(value: string): Promise<string> {
+      return 'hashed_password'
+    }
+  }
+
+  return new EncrypterStub()
+}
+
+type SutTypes = {
+  sut: DbAddAccount;
+  encrypterStub: Encrypter
+}
+function makeSut(): SutTypes {
+  const encrypterStub = makeEncrypter()
+  const sut = new DbAddAccount(encrypterStub)
+
+  return {
+    sut,
+    encrypterStub
+  }
+}
 
 describe('DbAddAccount Use Case', () => {
   test('Should call Encrypter with correct password', async () => {
-    class EncrypterStub {
-      async encrypt(value: string): Promise<string> {
-        return 'hashed_password'
-      }
-    }
 
-    const encrypterStub = new EncrypterStub()
-    const sut = new DbAddAccount(encrypterStub)
+    const { sut, encrypterStub } = makeSut()
+
     const encryptSpy = jest.spyOn(encrypterStub, 'encrypt')
 
     const accountData = {
