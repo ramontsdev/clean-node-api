931fe3b8e737c61231ed35b3144a81ba07d6e9ba
Ramon Nunes <ramontsdev@gmail.com>
2023-02-08 13:49:34 -0300

    chore: add jest confis for mongodb

--- jest-integration-config.js
+++ jest-integration-config.js
@@ -4,6 +4,7 @@ module.exports = {
   collectCoverage: false,
   coverageDirectory: "coverage",
   coverageProvider: "v8",
+  preset: '@shelf/jest-mongodb',
   transform: {
     '.+\\.ts$': 'ts-jest'
   },
--- jest.config.ts
+++ jest.config.ts
@@ -4,6 +4,7 @@ export default {
   collectCoverage: false,
   coverageDirectory: "coverage",
   coverageProvider: "v8",
+  preset: '@shelf/jest-mongodb',
   transform: {
     '.+\\.ts$': 'ts-jest'
   }
