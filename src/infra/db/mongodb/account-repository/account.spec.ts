ecbb1bf89ff18f7826aaa90e9dceb690c7d93eb7
Ramon Nunes <ramontsdev@gmail.com>
2023-02-08 14:53:58 -0300

    refactor: clear database between tests

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -10,8 +10,17 @@ describe('Account Mongo Repository', () => {
     await MongoHelper.disconnect()
   })
 
+  beforeEach(async () => {
+    const accountCollection = MongoHelper.getCollection('accounts')
+    accountCollection.deleteMany({})
+  })
+
+  function makeSut(): AccountMongoRepository {
+    return new AccountMongoRepository()
+  }
+
   test('Should return an account on success', async () => {
-    const sut = new AccountMongoRepository('')
+    const sut = makeSut()
 
     const account = await sut.add({
       name: 'any_name',
--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -4,8 +4,6 @@ import { AddAccountModel } from "../../../../domain/use-cases/add-account";
 import { MongoHelper } from "../helpers/mongo-helper";
 
 export class AccountMongoRepository implements AddAccountRepository {
-  constructor(nada: string) { }
-
   async add(accountData: AddAccountModel): Promise<AccountModel> {
     const accountCollection = MongoHelper.getCollection('accounts')
     const result = await accountCollection.insertOne(accountData)
