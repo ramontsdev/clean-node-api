e44a6f04327632f59ed829e6fc605df99928a173
Ramon Nunes <ramontsdev@gmail.com>
2023-02-09 15:30:56 -0300

    factory logMongoRepository

--- src/main/factories/sign-up.ts
+++ src/main/factories/sign-up.ts
@@ -1,6 +1,7 @@
 import { DbAddAccount } from '../../data/use-cases/add-account/db-add-account'
 import { BcryptAdapter } from '../../infra/cryptography/bcrypt-adapter'
 import { AccountMongoRepository } from '../../infra/db/mongodb/account-repository/account'
+import { LogMongoRepository } from '../../infra/db/mongodb/log-repository/log'
 import { SignUpController } from '../../presentation/controllers/sign-up/sign-up-controller'
 import { Controller } from '../../presentation/protocols'
 import { EmailValidatorAdapter } from '../../utils/email-validator-adapter'
@@ -15,6 +16,7 @@ export function makeSignUpController(): Controller {
   const dbAddAccount = new DbAddAccount(encrypterAdapter, accountMongoRepository)
 
   const signUpController = new SignUpController(emailValidatorAdapter, dbAddAccount)
+  const logMongoRepository = new LogMongoRepository()
 
-  return new LogControllerDecorator(signUpController)
+  return new LogControllerDecorator(signUpController, logMongoRepository)
 }
