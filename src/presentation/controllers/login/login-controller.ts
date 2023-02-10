08808445000b5f5444fb7714c677ecb2ba0b7a7f
Ramon Nunes <ramontsdev@gmail.com>
2023-02-10 10:07:45 -0300

    Should call Authentication with correct values

--- src/presentation/controllers/login/login-controller.ts
+++ src/presentation/controllers/login/login-controller.ts
@@ -1,3 +1,4 @@
+import { Authentication } from "../../../domain/use-cases/authentication";
 import { InvalidParamError, MissingParamError } from "../../errors";
 import { badRequest, serverError } from "../../helpers/http-helper";
 import { Controller, HttpRequest, HttpResponse } from "../../protocols";
@@ -5,7 +6,10 @@ import { EmailValidator } from "../sign-up/sign-up-protocols";
 
 export class LoginController implements Controller {
 
-  constructor(private readonly emailValidator: EmailValidator) { }
+  constructor(
+    private readonly emailValidator: EmailValidator,
+    private readonly authentication: Authentication
+  ) { }
 
   // @ts-ignore
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
@@ -23,6 +27,9 @@ export class LoginController implements Controller {
       if (!isValid) {
         return badRequest(new InvalidParamError('email'))
       }
+
+      await this.authentication.auth(email, password)
+
     } catch (error) {
       return serverError(error as Error)
     }
--- src/presentation/controllers/login/login.spec.ts
+++ src/presentation/controllers/login/login.spec.ts
@@ -1,3 +1,4 @@
+import { Authentication } from "../../../domain/use-cases/authentication";
 import { InvalidParamError, MissingParamError } from "../../errors";
 import { badRequest, serverError } from "../../helpers/http-helper";
 import { EmailValidator } from "../sign-up/sign-up-protocols";
@@ -13,6 +14,16 @@ function makeEmailValidator(): EmailValidator {
   return new EmailValidatorStub()
 }
 
+function makeAuthentication(): Authentication {
+  class AuthenticationStub implements Authentication {
+    async auth(email: string, password: string): Promise<string> {
+      return 'any_token'
+    }
+  }
+
+  return new AuthenticationStub()
+}
+
 function makeFakeRequest() {
   return {
     body: {
@@ -25,13 +36,16 @@ function makeFakeRequest() {
 type SutTypes = {
   sut: LoginController;
   emailValidatorStub: EmailValidator;
+  authenticationStub: Authentication;
 }
 function makeSut(): SutTypes {
   const emailValidatorStub = makeEmailValidator()
-  const sut = new LoginController(emailValidatorStub)
+  const authenticationStub = makeAuthentication()
+  const sut = new LoginController(emailValidatorStub, authenticationStub)
   return {
     sut,
-    emailValidatorStub
+    emailValidatorStub,
+    authenticationStub
   }
 }
 
@@ -86,5 +100,13 @@ describe('Login Controller', () => {
 
     const httpResponse = await sut.handle(makeFakeRequest())
     expect(httpResponse).toEqual(serverError(new Error("")))
+  })
+
+  test('Should call Authentication with correct values', async () => {
+    const { sut, authenticationStub } = makeSut()
+    const authSpy = jest.spyOn(authenticationStub, 'auth')
+
+    await sut.handle(makeFakeRequest())
+    expect(authSpy).toHaveBeenCalledWith('any_email@mail.com', 'any_password')
   })
 })
