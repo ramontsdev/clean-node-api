d9dc388c2b611c4e7a9296175894b47b5c4faf07
Ramon Nunes <ramontsdev@gmail.com>
2023-02-13 22:06:49 -0300

    move data layer interfaces to folder

--- src/data/protocols/db/load-account-by-email-repository.ts
+++ src/data/protocols/db/load-account-by-email-repository.ts
@@ -1,4 +1,4 @@
-import { AccountModel } from "../use-cases/add-account/db-add-account-protocols";
+import { AccountModel } from "../../use-cases/add-account/db-add-account-protocols";
 
 export interface LoadAccountByEmailRepository {
   load(email: string): Promise<AccountModel>
