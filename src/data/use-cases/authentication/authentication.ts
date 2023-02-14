25ca59698da903a1305871909fafb8866e13c7b7
Ramon Nunes <ramontsdev@gmail.com>
2023-02-14 00:20:45 -0300

    Should throw if HashComparer throws

--- src/data/use-cases/authentication/authentication.ts
+++ src/data/use-cases/authentication/authentication.ts
@@ -1,11 +1,13 @@
 import { Authentication, AuthenticationModel } from "../../../domain/use-cases/authentication";
 import { HashComparer } from "../../protocols/cryptography/hash-comparer";
+import { TokenGenerator } from "../../protocols/cryptography/token-generator";
 import { LoadAccountByEmailRepository } from "../../protocols/db/load-account-by-email-repository";
 
 export class DbAuthentication implements Authentication {
   constructor(
     private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository,
-    private readonly hashComparer: HashComparer
+    private readonly hashComparer: HashComparer,
+    private readonly tokenGenerator: TokenGenerator,
   ) { }
 
   async auth(authentication: AuthenticationModel): Promise<string | null> {
@@ -13,6 +15,7 @@ export class DbAuthentication implements Authentication {
 
     if (account) {
       await this.hashComparer.compare(authentication.password, account.password)
+      await this.tokenGenerator.generate(account.id)
     }
 
     return null
--- src/data/use-cases/authentication/db-authenticaton.spec.ts
+++ src/data/use-cases/authentication/db-authenticaton.spec.ts
@@ -1,5 +1,6 @@
 import { AuthenticationModel } from "../../../domain/use-cases/authentication"
 import { HashComparer } from "../../protocols/cryptography/hash-comparer"
+import { TokenGenerator } from "../../protocols/cryptography/token-generator"
 import { LoadAccountByEmailRepository } from "../../protocols/db/load-account-by-email-repository"
 import { AccountModel } from "../add-account/db-add-account-protocols"
 import { DbAuthentication } from "./authentication"
@@ -42,20 +43,38 @@ function makeHashComparer(): HashComparer {
   return new HashComparerStub()
 }
 
+function makeTokenGenerator(): TokenGenerator {
+  class TokenGeneratorStub implements TokenGenerator {
+    async generate(id: string): Promise<string> {
+
+      return 'any_token'
+    }
+  }
+
+  return new TokenGeneratorStub()
+}
+
 type SutTypes = {
   sut: DbAuthentication;
   loadAccountByEmailRepositoryStub: LoadAccountByEmailRepository;
   hashComparerStub: HashComparer;
+  tokenGeneratorStub: TokenGenerator;
 }
 function makeSut(): SutTypes {
   const loadAccountByEmailRepositoryStub = makeLoadAccountByEmailRepository()
   const hashComparerStub = makeHashComparer()
-  const sut = new DbAuthentication(loadAccountByEmailRepositoryStub, hashComparerStub)
+  const tokenGeneratorStub = makeTokenGenerator()
+  const sut = new DbAuthentication(
+    loadAccountByEmailRepositoryStub,
+    hashComparerStub,
+    tokenGeneratorStub
+  )
 
   return {
     sut,
     loadAccountByEmailRepositoryStub,
-    hashComparerStub
+    hashComparerStub,
+    tokenGeneratorStub
   }
 }
 
@@ -122,5 +141,27 @@ describe('DbAuthentication Use case', () => {
     const accessToken = await sut.auth(makeFakeAuthentication())
 
     expect(accessToken).toBeNull()
+  })
+
+  test('Should call TokenGenerator with correct id', async () => {
+    const { sut, tokenGeneratorStub } = makeSut()
+
+    const generateSpy = jest.spyOn(tokenGeneratorStub, 'generate')
+
+    await sut.auth(makeFakeAuthentication())
+
+    expect(generateSpy).toHaveBeenCalledWith("any_id")
+  })
+
+  test('Should throw if HashComparer throws', async () => {
+    const { sut, tokenGeneratorStub } = makeSut()
+
+    jest.spyOn(tokenGeneratorStub, 'generate').mockReturnValueOnce(
+      new Promise((resolve, reject) => reject(new Error()))
+    )
+
+    const promise = sut.auth(makeFakeAuthentication())
+
+    expect(promise).rejects.toThrow()
   })
 })
