4061370561451e48f27d2023830249b6298f00af
Ramon Nunes <ramontsdev@gmail.com>
2023-02-15 10:02:27 -0300

    Should return 200 on login

--- src/main/routes/login-routes.ts
+++ src/main/routes/login-routes.ts
@@ -1,7 +1,10 @@
 import { Router } from "express";
+
 import { adaptRoute } from "../adapters/express/express-route-adapter";
+import { makeLoginController } from "../factories/login/login-factory";
 import { makeSignUpController } from "../factories/sign-up/sign-up-factory";
 
 export default function (router: Router): void {
   router.post('/sign-up', adaptRoute(makeSignUpController()))
+  router.post('/login', adaptRoute(makeLoginController()))
 }
