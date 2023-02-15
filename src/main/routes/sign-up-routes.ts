f82a9e150dc3728fc7357efc050cc0d8a2a1b1fc
Ramon Nunes <ramontsdev@gmail.com>
2023-02-15 09:42:55 -0300

    refactor

--- src/main/routes/sign-up-routes.ts
+++ src/main/routes/sign-up-routes.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { adaptRoute } from "../adapters/express-route-adapter";
+import { adaptRoute } from "../adapters/express/express-route-adapter";
 import { makeSignUpController } from "../factories/sign-up/sign-up-factory";
 
 export default function (router: Router): void {
