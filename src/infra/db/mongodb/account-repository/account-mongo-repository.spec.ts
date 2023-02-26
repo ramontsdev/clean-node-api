d7b95345338cf9128af2f416e60cfae97c7c37e0
Ramon Nunes <ramontsdev@gmail.com>
2023-02-26 20:56:08 -0300

    fix: Should return an account on loadByToken with if user if admin

--- src/infra/db/mongodb/account-repository/account-mongo-repository.spec.ts
+++ src/infra/db/mongodb/account-repository/account-mongo-repository.spec.ts
@@ -111,7 +111,7 @@ describe('Account Mongo Repository', () => {
       expect(account?.password).toEqual('any_password')
     })
 
-    test('Should return an account on loadByToken wit role', async () => {
+    test('Should return an account on loadByToken with admin role', async () => {
       const sut = makeSut()
 
       await accountCollection.insertOne({
@@ -119,10 +119,45 @@ describe('Account Mongo Repository', () => {
         email: 'any_email@mail.com',
         password: 'any_password',
         accessToken: 'any_token',
-        role: 'any_role'
+        role: 'admin'
       })
 
-      const account = await sut.loadByToken('any_token', 'any_role')
+      const account = await sut.loadByToken('any_token', 'admin')
+
+      expect(account).toBeTruthy()
+      expect(account?.id).toBeTruthy()
+      expect(account?.name).toEqual('any_name')
+      expect(account?.email).toEqual('any_email@mail.com')
+      expect(account?.password).toEqual('any_password')
+    })
+
+    test('Should return null on loadByToken with invalid role', async () => {
+      const sut = makeSut()
+
+      await accountCollection.insertOne({
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        accessToken: 'any_token',
+      })
+
+      const account = await sut.loadByToken('any_token', 'admin')
+
+      expect(account).toBeFalsy()
+    })
+
+    test('Should return an account on loadByToken with if user if admin', async () => {
+      const sut = makeSut()
+
+      await accountCollection.insertOne({
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        accessToken: 'any_token',
+        role: 'admin'
+      })
+
+      const account = await sut.loadByToken('any_token')
 
       expect(account).toBeTruthy()
       expect(account?.id).toBeTruthy()
--- src/infra/db/mongodb/account-repository/account-mongo-repository.ts
+++ src/infra/db/mongodb/account-repository/account-mongo-repository.ts
@@ -36,9 +36,16 @@ export class AccountMongoRepository
     })
   }
 
-  async loadByToken(token: string, role?: string | undefined): Promise<AccountModel | null> {
+  async loadByToken(token: string, role?: string): Promise<AccountModel | null> {
     const accountCollection = await MongoHelper.getCollection('accounts')
-    const account = await accountCollection.findOne({ accessToken: token, role })
+    const account = await accountCollection.findOne({
+      accessToken: token,
+      $or: [{
+        role
+      }, {
+        role: 'admin'
+      }]
+    })
     return account && MongoHelper.map(account)
   }
 }
