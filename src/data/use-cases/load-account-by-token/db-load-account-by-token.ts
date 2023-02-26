c106bc1fdf0af91aa4934d4a836df8f0a7929583
Ramon Nunes <ramontsdev@gmail.com>
2023-02-26 20:47:02 -0300

    Should return 204 on add survey with valid accessToken

--- src/data/use-cases/load-account-by-token/db-load-account-by-token.ts
+++ src/data/use-cases/load-account-by-token/db-load-account-by-token.ts
@@ -9,10 +9,10 @@ export class DbLoadAccountByToken implements LoadAccountByToken {
     private readonly loadAccountByTokenRepository: LoadAccountByTokenRepository
   ) { }
 
-  async load(accessToken: string, role?: string | undefined): Promise<AccountModel | null> {
+  async load(accessToken: string, role?: string): Promise<AccountModel | null> {
     const token = await this.decrypter.decrypt(accessToken)
     if (token) {
-      const account = await this.loadAccountByTokenRepository.loadByToken(token, role)
+      const account = await this.loadAccountByTokenRepository.loadByToken(accessToken, role)
       if (account) {
         return account
       }
