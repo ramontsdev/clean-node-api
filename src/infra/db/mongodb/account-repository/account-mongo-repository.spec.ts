591d003d7ed69acfc49cef2743314f2e8329a043
Ramon Nunes <ramontsdev@gmail.com>
2023-02-25 22:14:01 -0300

    Should return null if loadByToken fails

--- src/infra/db/mongodb/account-repository/account-mongo-repository.spec.ts
+++ src/infra/db/mongodb/account-repository/account-mongo-repository.spec.ts
@@ -110,5 +110,33 @@ describe('Account Mongo Repository', () => {
       expect(account?.email).toEqual('any_email@mail.com')
       expect(account?.password).toEqual('any_password')
     })
+
+    test('Should return an account on loadByToken wit role', async () => {
+      const sut = makeSut()
+
+      await accountCollection.insertOne({
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        accessToken: 'any_token',
+        role: 'any_role'
+      })
+
+      const account = await sut.loadByToken('any_token', 'any_role')
+
+      expect(account).toBeTruthy()
+      expect(account?.id).toBeTruthy()
+      expect(account?.name).toEqual('any_name')
+      expect(account?.email).toEqual('any_email@mail.com')
+      expect(account?.password).toEqual('any_password')
+    })
+
+    test('Should return null if loadByToken fails', async () => {
+      const sut = makeSut()
+
+      const account = await sut.loadByEmail('any_token')
+
+      expect(account).toBeFalsy()
+    })
   })
 })
