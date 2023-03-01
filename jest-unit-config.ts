cab3723ebd7a207ed6c38732af7a0f73e83400ec
Ramon Nunes <ramontsdev@gmail.com>
2023-03-01 12:15:33 -0300

    type alias

--- jest-unit-config.ts
+++ jest-unit-config.ts
@@ -1,21 +1,5 @@
 import jestConfig from './jest.config';
 
-//module.exports = {
-//  roots: ['<rootDir>/src'],
-//  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
-//  collectCoverage: false,
-//  coverageDirectory: "coverage",
-//  coverageProvider: "v8",
-//  preset: '@shelf/jest-mongodb',
-//  transform: {
-//    '.+\\.ts$': 'ts-jest'
-//  },
-//  testMatch: ['**/*.spec.ts'],
-//  moduleNameMapper: {
-//    '@/(.*)': '<rootDir>/src/$1'
-//  }
-//};
-
 export default {
   ...jestConfig,
   testMatch: ['**/*.spec.ts'],
