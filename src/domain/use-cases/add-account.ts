73832974bdf9f6979850616bf34c7ac3045f8581
Ramon Nunes <ramontsdev@gmail.com>
2023-03-01 12:34:21 -0300

    refactor: use feat Omit of Typescrip

--- src/domain/use-cases/add-account.ts
+++ src/domain/use-cases/add-account.ts
@@ -1,10 +1,12 @@
 import { AccountModel } from "@/domain/models/account";
 
-export type AddAccountModel = {
+export type AddAccountModel = Omit<AccountModel, 'id'>
+
+/* export type AddAccountModel = {
   name: string;
   email: string;
   password: string;
-}
+} */
 
 export interface AddAccount {
   add(account: AddAccountModel): Promise<AccountModel | null>
